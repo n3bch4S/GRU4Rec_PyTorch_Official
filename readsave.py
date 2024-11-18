f230289964a714e7d460f9e78000c5a9f1af3a9d
Thada Srisaluaikun <gnar875@gmail.com>
2024-11-19 00:43:56 +0700

    update readsave

--- readsave.py
+++ readsave.py
@@ -4,7 +4,7 @@ import joblib
 
 read_params = [
     {
-        "filepath_or_buffer": "dataset/yoochoose-clicks.dat",
+        "filepath_or_buffer": "dataset/yoochoose-clicks-200k.dat",
         "usecols": ["SessionId", "Time", "ItemId"],
         "dtype": {
             "SessionId": np.int32,
@@ -13,7 +13,7 @@ read_params = [
         "parse_dates": ["Time"],
     },
     {
-        "filepath_or_buffer": "dataset/yoochoose-test.dat",
+        "filepath_or_buffer": "dataset/yoochoose-test-200k.dat",
         "usecols": ["SessionId", "Time", "ItemId"],
         "dtype": {
             "SessionId": np.int32,
@@ -24,17 +24,35 @@ read_params = [
 ]
 
 write_params = [
-    {"filename": "dataset/yoochoose-clicks.pickle"},
-    {"filename": "dataset/yoochoose-test.pickle"},
+    {"filename": "dataset/yoochoose-clicks-200k.pickle"},
+    {"filename": "dataset/yoochoose-test-200k.pickle"},
 ]
 
-for i in range(len(read_params)):
-    read_param, write_param = read_params[i], write_params[i]
 
-    print(f"read {read_param}...\n")
-    df = pd.read_csv(**read_param)
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    # Sessions of length 1 are filtered out (both in training and test sets)
+    print("Preprocessing...")
 
-    print(f"write {write_param}...\n")
-    joblib.dump(df, **write_param)
+    df = df.groupby("SessionId").filter(lambda x: len(x) > 1)
+    df.info()
 
-    print(f"done\n\n\n")
+    print("Done Preprocessing")
+
+
+def main() -> None:
+    for i in range(len(read_params)):
+        read_param, write_param = read_params[i], write_params[i]
+
+        print(f"read {read_param}...\n")
+        df = pd.read_csv(**read_param)
+
+        preprocess(df)
+
+        print(f"write {write_param}...\n")
+        joblib.dump(df, **write_param)
+
+        print(f"done\n\n")
+
+
+if __name__ == "__main__":
+    main()
